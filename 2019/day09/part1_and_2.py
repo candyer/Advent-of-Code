0c24798253732038c2849dfdf80dbc632559d16e
c <candyer@users.noreply.github.com>
2019-12-09 22:11:54 +0100

    Day 9: Sensor Boost

--- 2019/day09/part1_and_2.py
+++ 2019/day09/part1_and_2.py
@@ -1,5 +1,5 @@
 import sys
-from typing import List, Tuple
+from typing import List, Tuple, Dict
 from collections import defaultdict
 
 def breakDown(num: int) -> Tuple[int, List[int]]:
@@ -7,65 +7,65 @@ def breakDown(num: int) -> Tuple[int, List[int]]:
 	modes.extend([0] * (3 - len(modes)))
 	return (num % 100, modes)
 
-def solve(arr: List[int], inpt: int) -> int:
+def solve(d: Dict[int, int], inpt: int) -> int:
 	relative_base = 0
 	output = 0
 	i = 0
 
 	while True:
-		opcode, modes = breakDown(arr[i])
+		opcode, modes = breakDown(d[i])
 		if opcode == 99:
 			return output
 
-		a = i + 1 if modes[0] == 1 else arr[i + 1] if modes[0] == 0 else arr[i + 1] + relative_base
-		b = i + 2 if modes[1] == 1 else arr[i + 2] if modes[1] == 0 else arr[i + 2] + relative_base
-		c = arr[i + 3] if modes[2] == 0 else arr[i + 3] + relative_base
+		a = i + 1 if modes[0] == 1 else d[i + 1] if modes[0] == 0 else d[i + 1] + relative_base
+		b = i + 2 if modes[1] == 1 else d[i + 2] if modes[1] == 0 else d[i + 2] + relative_base
+		c = d[i + 3] if modes[2] == 0 else d[i + 3] + relative_base
 
 		############################################################
 		if opcode == 1:
-			arr[c] = arr[a] + arr[b]
+			d[c] = d[a] + d[b]
 			i += 4
 
 		elif opcode == 2:
-			arr[c] = arr[a] * arr[b]
+			d[c] = d[a] * d[b]
 			i += 4
 
 		elif opcode == 3:
-			arr[a] = inpt
+			d[a] = inpt
 			i += 2
 
 		elif opcode == 4:
 			i += 2
-			output = arr[a]
+			output = d[a]
 
 		elif opcode == 5:
-			if arr[a]:
-				i = arr[b]
+			if d[a]:
+				i = d[b]
 			else:
 				i += 3
 
 		elif opcode == 6:
-			if arr[a]:
+			if d[a]:
 				i += 3
 			else:
-				i = arr[b]
+				i = d[b]
 
 		elif opcode == 7:
-			if arr[a] < arr[b]:
-				arr[c] = 1
+			if d[a] < d[b]:
+				d[c] = 1
 			else:
-				arr[c] = 0
+				d[c] = 0
 			i += 4
 
 		elif opcode == 8:
-			if arr[a] == arr[b]:
-				arr[c] = 1
+			if d[a] == d[b]:
+				d[c] = 1
 			else:
-				arr[c] = 0
+				d[c] = 0
 			i += 4
 
 		elif opcode == 9:
-			relative_base += arr[a]
+			relative_base += d[a]
 			i += 2
 
 
