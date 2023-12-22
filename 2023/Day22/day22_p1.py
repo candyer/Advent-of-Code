598a588f32b233fbe75f9bf12bec6357e1f53750
J C <candyer@users.noreply.github.com>
2023-12-23 00:30:25 +0100

    Add files via upload

---
+++ 2023/Day22/day22_p1.py
@@ -0,0 +1,62 @@
+# https://adventofcode.com/2023/day/22
+
+######################
+### -- part 1 -- #####
+######################
+
+import sys
+from collections import defaultdict
+
+def solve(bricks):
+	bricks.sort(key=lambda x: x[0][2])
+	basalt = defaultdict(lambda: (0, 0)) # (height "z", id of the thing "floor")
+	above_me = defaultdict(set)
+	below_me = defaultdict(set)
+
+	idx = 1
+	for brick in bricks:
+		(x1, y1, z1), (x2, y2, z2) = brick
+		assert x1 <= x2
+		assert y1 <= y2
+		assert z1 <= z2
+		# find tallest spot bellow me
+		tallest = 0
+		for x in range(x1, x2 + 1):
+			for y in range(y1, y2 + 1):
+				val, _ = basalt[(x, y)]
+				tallest = max(tallest, val)
+		for x in range(x1, x2 + 1):
+			for y in range(y1, y2 + 1):
+				val, surface = basalt[(x, y)]
+				if val == tallest:
+					above_me[surface].add(idx)
+					below_me[idx].add(surface)
+		# update the world after I land
+		tallest += (z2 - z1) + 1
+		for x in range(x1, x2 + 1):
+			for y in range(y1, y2 + 1):
+				basalt[(x, y)] = (tallest, idx)
+		idx += 1
+
+	# can we delete
+	count = 0
+	for surface, aboves in above_me.items():
+		if surface == 0: continue # i don't want to handle the floor
+		count += all(len(below_me[above]) > 1 for above in aboves)
+	return count + len(bricks) - len(above_me) + 1
+
+
+def main():
+	bricks = []
+	for line in sys.stdin:
+		left, right = line.strip('\n').split('~')
+		# print(f'{left=}')
+		left = list(map(int, left.split(',')))
+		right = list(map(int, right.split(',')))
+		bricks.append([left, right])
+	print(solve(bricks))
+
+if __name__ == '__main__':
+	main()
+
+
